58a84b7f45093107a79b5f2a3f99a1236fe69324
Angelique Nehmzow <angelique@nehmzow.com>
2019-11-21 10:05:18 -0800

    Make endpoint for getting data from bpi api, use chart.js to display data

--- challenge_2/client/App.jsx
+++ challenge_2/client/App.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import Chart from "chart.js";
 
-class App extends React.Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      labels: [],
+      data: []
+    };
+    this.myChart = React.createRef();
+  }
+
+  buildChart() {
+    let ctx = this.myChart.current.getContext('2d');
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: this.state.labels,
+        datasets: [
+          {
+            label: 'BTC',
+            data: this.state.data
+          }
+        ]
+      }
+    });
+  }
+
+  componentDidMount() {
+    $.get(`/data`, (response) => {
+      this.setState({
+        labels: response.labels,
+        data: response.data
+      }, this.buildChart);
+    });
+  }
+
   render() {
-    return <h1>Hello!</h1>;
+    return (
+      <div>
+        <h1>Cryptocurrency Charting Tool</h1>
+        <canvas ref={this.myChart} />
+      </div>
+    )
   }
 }
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,9 +2,32 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const morgan = require('morgan');
+const request = require('request');
 
 app.use(express.static('public'))
 app.use(express.json());
 app.use(morgan('dev'));
+
+app.get('/data', (req, res) => {
+  let url = 'https://api.coindesk.com/v1/bpi/historical/close.json';
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log('Error:', error);
+      return res.status(500).end();
+    }
+    let origData = JSON.parse(body).bpi;
+    let labels = [];
+    let data = [];
+    for (let date in origData) {
+      labels.push(date);
+      data.push(origData[date]);
+    }
+    let newData = {
+      labels: labels,
+      data: data
+    };
+    res.send(newData);
+  });
+})
 
 app.listen(PORT, () => console.log(`App server listening on port ${PORT}`));
