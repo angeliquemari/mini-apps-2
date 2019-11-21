2104217e78c871673380e1f31e7293f8c55778d8
Angelique Nehmzow <angelique@nehmzow.com>
2019-11-20 19:39:33 -0800

    Set up webpack bundling, express server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const morgan = require('morgan');
 
+app.use(express.static('public'))
+app.use(express.json());
+app.use(morgan('dev'));
 
+app.listen(PORT, () => console.log(`App server listening on port ${PORT}`));
---
+++ challenge_2/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/App.jsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
